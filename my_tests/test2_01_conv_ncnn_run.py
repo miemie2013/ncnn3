c7a0b458a57542ad804aba8563c88f3f1d05af8d
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-07-27 19:04:42 +0800

    f

--- my_tests/test2_01_conv_ncnn_run.py
+++ my_tests/test2_01_conv_ncnn_run.py
@@ -1,6 +1,15 @@
 
 '''
 （在pycharm的普通终端输入）：
+cd build/examples
+.\test2_01_conv_ncnn ../../my_tests/my_test.jpg ../../my_tests/01.param ../../my_tests/01.bin
+
+(linux)
+cd build/examples
+./test2_01_conv_ncnn ../../my_tests/my_test.jpg ../../my_tests/01.param ../../my_tests/01.bin
+
+
+（在pycharm的普通终端输入）：
 cd my_tests
 ../tools/pnnx/build/install/bin/pnnx 01.pt inputshape=[1,3,4,4]
 
@@ -12,7 +21,6 @@ cd my_tests
 (预测)
 cd build/examples
 .\test2_01_conv_ncnn ../../my_tests/my_test.jpg ../../my_tests/01.ncnn.param ../../my_tests/01.ncnn.bin
-
 
 
 '''
