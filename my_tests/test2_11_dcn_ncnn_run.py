0f3aa03cd63bb5972445af0cc7868fb13a22ef48
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2022-07-27 18:53:17 +0800

    f

---
+++ my_tests/test2_11_dcn_ncnn_run.py
@@ -0,0 +1,18 @@
+
+'''
+（在pycharm的普通终端输入）：
+cd build/examples
+.\test2_01_conv_ncnn ../../my_tests/my_test32.jpg ../../my_tests/11_pncnn.param ../../my_tests/11_pncnn.bin
+
+.\test2_01_conv_ncnn ../../my_tests/my_test2.jpg ../../my_tests/11_pncnn.param ../../my_tests/11_pncnn.bin
+
+.\test2_01_conv_ncnn ../../my_tests/my_test9_7.jpg ../../my_tests/11_pncnn.param ../../my_tests/11_pncnn.bin
+
+(linux)
+cd build/examples
+./test2_01_conv_ncnn ../../my_tests/my_test32.jpg ../../my_tests/11_pncnn.param ../../my_tests/11_pncnn.bin
+
+'''
+
+
+
